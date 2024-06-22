5c3d0b1e256341e1ff66c02d3c973e49358905e2
niggative <andrei2004bekman@gmail.com>
2024-06-22 13:49:12 +0300

    apteki on map

--- HomePharmacy/HomePharmacy/Map.xaml.cs
+++ HomePharmacy/HomePharmacy/Map.xaml.cs
@@ -17,7 +17,7 @@ public partial class Map : ContentPage
         {
 
             Location location = await Geolocation.Default.GetLocationAsync();
-
+            
             if (location != null)
             {
                 Maui.GoogleMaps.Position myposition = new Maui.GoogleMaps.Position(location.Latitude, location.Longitude);
@@ -32,15 +32,115 @@ public partial class Map : ContentPage
                                )));
                 Pin _pinA = new Pin()
                 {
-                    Icon = BitmapDescriptorFactory.FromBundle("pin2"), //don't use extension
+                    Icon = BitmapDescriptorFactory.FromBundle("human1"), //don't use extension
                     Type = PinType.Place,
-                    Label = "Tokyo SKYTREE",
-                    Address = "Sumida-ku, Tokyo, Japan",
+                    Label = "My position",
+                    Address = myposition.Latitude.ToString() + myposition.Longitude.ToString(),
                     Position = myposition
                 
-                
+        
                 };
+                mymap.Pins.Add(new Pin
+                {
+                    Icon = BitmapDescriptorFactory.FromBundle("pin2"),
+                    Label = "Атека №103",
+                    Address = "ул. Гамарника 16",
+                    Position = new Position(53.95950452893426, 27.616673083899546)
+                });
+                mymap.Pins.Add(new Pin
+                {
+                    Icon = BitmapDescriptorFactory.FromBundle("pin2"),
+                    Label = "Атека",
+                    Address = "Логойский тракт 7, Минск, Минская область, Беларусь",
+                    Position = new Position(53.94408248768406, 27.605613162421353)
+                });
+                mymap.Pins.Add(new Pin
+                {
+                    Icon = BitmapDescriptorFactory.FromBundle("pin2"),
+                    Label = "Аптека ADEL №2",
+                    Address = "ул. Куйбышева д. 40, Минск, Минская область, Беларусь",
+                    Position = new Position(53.92426599323242, 27.578925091521043)
+                });
+                mymap.Pins.Add(new Pin
+                {
+                    Icon = BitmapDescriptorFactory.FromBundle("pin2"),
+                    Label = "Аптека Планета Здоровья",
+                    Address = "пр. Независимости 48, Минск, Минская область 220089, Беларусь",
+                    Position = new Position(53.9154871099015, 27.579405957874535)
+                });
+                mymap.Pins.Add(new Pin
+                {
+                    Icon = BitmapDescriptorFactory.FromBundle("pin2"),
+                    Label = "Столичная аптека №18 Самбест",
+                    Address = "ул. Романовская Слобода 13, пом.2Н, Минск, Минская область, Беларусь",
+                    Position = new Position(53.908264386941276, 27.53949406732279)
+                });
+                mymap.Pins.Add(new Pin
+                {
+                    Icon = BitmapDescriptorFactory.FromBundle("pin2"),
+                    Label = "Аптека Планета Здоровья",
+                    Address = "пр. Независимости 18, Минск, Минская область, Беларусь",
+                    Position = new Position(53.902173669496605, 27.55848828029621)
+                });
+                mymap.Pins.Add(new Pin
+                {
+                    Icon = BitmapDescriptorFactory.FromBundle("pin2"),
+                    Label = "Аптека",
+                    Address = "улица Пулихова 1, Минск, Минская область, Беларусь",
+                    Position = new Position(53.90146538890196, 27.573395130984213)
+                });
+                mymap.Pins.Add(new Pin
+                {
+                    Icon = BitmapDescriptorFactory.FromBundle("pin2"),
+                    Label = "Аптека",
+                    Address = "ул. Долгобродская 7/1, Минск Телефон: 8 017 245-31-11 - телефон не существует, ул. Долгобродская 7, Минск, Беларусь",
+                    Position = new Position(53.90429843923455, 27.600083202377245)
+                });
+                mymap.Pins.Add(new Pin
+                {
+                    Icon = BitmapDescriptorFactory.FromBundle("pin2"),
+                    Label = "Аптека",
+                    Address = "бульвар Тракторостроителей 5, Минск, Минская область, Беларусь",
+                    Position = new Position(53.89112312471876, 27.614509186914024)
+                });
+                mymap.Pins.Add(new Pin
+                {
+                    Icon = BitmapDescriptorFactory.FromBundle("pin2"),
+                    Label = "Аптека Белфармация",
+                    Address = "ул. Воронянского 11/5, Минск, Минская область, Беларусь",
+                    Position = new Position(53.88389618803909, 27.54286013057929)
+                });
+                mymap.Pins.Add(new Pin
+                {
+                    Icon = BitmapDescriptorFactory.FromBundle("pin2"),
+                    Label = "Аптека № 8 Белфармация",
+                    Address = "улица Чкалова 17, Минск, Минская область, Беларусь",
+                    Position = new Position(53.87893580282909, 27.533723673706)
+                });
+                mymap.Pins.Add(new Pin
+                {
+                    Icon = BitmapDescriptorFactory.FromBundle("pin2"),
+                    Label = "Аптека Планета Здоровья",
+                    Address = "улица Маяковского 146, Минск, Минская область, Беларусь",
+                    Position = new Position(53.8748253235276, 27.569788635047942)
+                });
+                mymap.Pins.Add(new Pin
+                {
+                    Icon = BitmapDescriptorFactory.FromBundle("pin2"),
+                    Label = "Аптека Белфармация",
+                    Address = "улица Маяковского 146, Минск, Минская область, Беларусь",
+                    Position = new Position(53.92219461275831, 27.4793713042543)
+                });
+                mymap.Pins.Add(new Pin
+                {
+                    Icon = BitmapDescriptorFactory.FromBundle("pin2"),
+                    Label = "Белфармация Аптека № 91",
+                    Address = "улица Маяковского 146, Минск, Минская область, Беларусь",
+                    Position = new Position(53.90410274573088, 27.43528862354487)
+                });
+
                 mymap.Pins.Add(_pinA);
+                //mymap.Pins.Add(_pinA);
             }
 
         }
