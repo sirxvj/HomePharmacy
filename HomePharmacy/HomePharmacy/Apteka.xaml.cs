44027abf184dafbd49ad907d93bf03120e49149b
niggative <andrei2004bekman@gmail.com>
2024-06-22 14:36:54 +0300

    apteki details

--- HomePharmacy/HomePharmacy/Apteka.xaml.cs
+++ HomePharmacy/HomePharmacy/Apteka.xaml.cs
@@ -10,6 +10,10 @@ public partial class Apteka : ContentPage
     private async void OnAddClick(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync("AddApteku");
-
+    }
+    private async void OnAptekaClick(object sender, EventArgs e)
+    {
+        //await Shell.Current.GoToAsync("AddApteku");
+        await Navigation.PushAsync(new AptekaDetails());
     }
 }
---
+++ HomePharmacy/HomePharmacy/ReceptDetails.xaml.cs
@@ -0,0 +1,9 @@
+namespace HomePharmacy;
+
+public partial class ReceptDetails : ContentPage
+{
+	public ReceptDetails()
+	{
+		InitializeComponent();
+	}
+}
